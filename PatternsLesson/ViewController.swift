caf9f39a976e2962aff72b8c6a85beba78e07d02
Ildar Zalyalov <ildarzalyalovmos@gmail.com>
2020-03-16 14:41:46 +0300

    Initial Commit

---
+++ PatternsLesson/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PatternsLesson
+//
+//  Created by Ильдар Залялов on 16.03.2020.
+//  Copyright © 2020 Ildar Zalyalov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
