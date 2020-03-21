1bbc359e4324690884571c2c3d09e0e37ba91ba5
enoxus <reqenoxus@gmail.com>
2020-03-21 11:18:17 +0300

    removed redundant colors

--- PatternsLesson/Common/Constants/Colors.swift
+++ PatternsLesson/Common/Constants/Colors.swift
@@ -10,16 +10,8 @@ import Foundation
 import UIKit
 
 extension UIColor {
-
-    @nonobjc class var blackBg: UIColor {
-      return UIColor(white: 30.0 / 255.0, alpha: 1.0)
-    }
     
     @nonobjc class var orangeUi: UIColor {
         return UIColor(red: 255.0 / 255.0, green: 159.0 / 255.0, blue: 10.0 / 255.0, alpha: 1)
-    }
-    
-    @nonobjc class var paleGrey: UIColor {
-      return UIColor(red: 235.0 / 255.0, green: 235.0 / 255.0, blue: 245.0 / 255.0, alpha: 1)
     }
 }
