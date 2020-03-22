0a96ded0be8e00541c93d9a355294ecd23e02ada
Nikita <luzhbinnikita@gmail.com>
2020-03-22 17:02:40 +0300

    Added basic VIPER structure

---
+++ Lesson_12/Modules/Login/View/LoginView.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var textFieldsView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
